ff57fadc657b72ab1ce234d4fb94a75febb0521b
unknown <ridwangmrz@gmail.com>
2025-07-02 13:56:04 +0700

    Final

--- src/store/penjualan.js
+++ src/store/penjualan.js
@@ -9,7 +9,7 @@ export const usePenjualanStore = defineStore('penjualan', {
   actions: {
     async fetchPenjualan() {
       try {
-        const res = await axios.get('const response = await axios.get('https:/my-json-server.typicode.com/<RDN-10>>/pbk10-ridwan/penjualan');')
+        const res = await axios.get('https://my-json-server.typicode.com/RDN-10/pbk10-ridwan/penjualan')
         this.daftarPenjualan = res.data
       } catch (err) {
         console.error('Gagal fetch penjualan:', err.message)
